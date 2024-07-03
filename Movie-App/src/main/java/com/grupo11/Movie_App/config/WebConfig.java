eb3ba44683f425a01057d63d10ff2153a00e3f47
Fernando Rodriguez <ferchulobo2015@gmail.com>
2024-07-03 11:59:36 -0300

    configuracion global cors

---
+++ Movie-App/src/main/java/com/grupo11/Movie_App/config/WebConfig.java
@@ -0,0 +1,24 @@
+package com.grupo11.Movie_App.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("https://ferchulobo777-movie-app.vercel.app");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
