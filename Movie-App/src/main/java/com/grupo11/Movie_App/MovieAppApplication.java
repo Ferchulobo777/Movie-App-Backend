cccaca9b97607cb7b75903035c1e8afcdc00e880
Fernando Rodriguez <ferchulobo2015@gmail.com>
2024-07-03 14:33:50 -0300

    fix cors

--- Movie-App/src/main/java/com/grupo11/Movie_App/MovieAppApplication.java
+++ Movie-App/src/main/java/com/grupo11/Movie_App/MovieAppApplication.java
@@ -19,7 +19,7 @@ public class MovieAppApplication {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("/**")
+						.allowedOrigins("*")
 						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 						.allowedHeaders("*")
 						.allowCredentials(true);
