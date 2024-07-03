1559a5ba5b580a2b1a259a612bcfd91c0d551b1a
Fernando Rodriguez <ferchulobo2015@gmail.com>
2024-07-03 17:51:39 -0300

    fix cors

--- Movie-App/src/main/java/com/grupo11/Movie_App/MovieAppApplication.java
+++ Movie-App/src/main/java/com/grupo11/Movie_App/MovieAppApplication.java
@@ -2,28 +2,11 @@ package com.grupo11.Movie_App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MovieAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieAppApplication.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("https://ferchulobo777-movie-app.netlify.app", "http://127.0.0.1:5500")
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-						.allowedHeaders("*")
-						.allowCredentials(true);
-			}
-		};
 	}
 }
